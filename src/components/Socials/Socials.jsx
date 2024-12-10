a14f8ed76c4a91197a6dd1c4d0f1b1769f3bac6a
ujjwal-dahal <toppelp@gmail.com>
2024-12-10 11:53:40 +0545

    Home Page Social Icons URL Link has been Updated

--- src/components/Socials/Socials.jsx
+++ src/components/Socials/Socials.jsx
@@ -1,23 +1,23 @@
 import React from 'react'
-import {FaGithub , FaLinkedinIn , FaYoutube , FaTwitter} from "react-icons/fa";
+import {FaGithub , FaLinkedinIn , FaYoutube , FaFacebook} from "react-icons/fa";
 import Link from 'next/link';
 
 
 const socialMediaIcons = [
-  {icon : <FaGithub /> , path : ""},
-  {icon : <FaLinkedinIn /> , path : ""},
-  {icon : <FaYoutube /> , path : ""},
-  {icon : <FaTwitter /> , path : ""},
+  {icon : <FaGithub /> , path : "https://github.com/ujjwal-dahal"},
+  {icon : <FaLinkedinIn /> , path : "https://www.linkedin.com/in/brightnessujjwal/"},
+  {icon : <FaYoutube /> , path : "https://www.youtube.com/@BrightnessVlogs"},
+  {icon : <FaFacebook /> , path : "https://www.facebook.com/brightness.ujjwal"},
 ]
 
 
 const Socials = ({containerStyles , iconStyles}) => {
   return (
-    <div className={containerStyles}>
+    <div className={containerStyles} key={containerStyles}>
       {
         socialMediaIcons.map((Icon , index)=>{
           return <>
-            <Link href={Icon.path} key={index} className={iconStyles}>
+            <Link href={Icon.path} target='_blank' key={index} className={iconStyles}>
               {Icon.icon}
             </Link>
           </>
